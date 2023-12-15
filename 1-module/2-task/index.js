ca5d18e53542d763bd0d5009eb0065c144a903e8
Ivan <ivonchik996@gmail.com>
2023-12-15 15:16:49 +0300

    add 1-module-2-task

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -1,24 +1,19 @@
-/**
- * Эту функцию трогать не нужно
- */
 function print(text) {
   console.log(text);
 }
 
-/**
- * Эту функцию нужно поменять так,
- * чтобы функция sayHello работала корректно
- */
 function isValid(name) {
-  // ваш код...
+  return name !== null && !name.includes(" ") && name.length > 3 ? true : false;
 }
 
 function sayHello() {
-  let userName = prompt('Введите ваше имя');
+  let userName = prompt("Введите ваше имя");
 
   if (isValid(userName)) {
     print(`Welcome back, ${userName}!`);
   } else {
-    print('Некорректное имя');
+    print("Некорректное имя");
   }
 }
+
+sayHello();
