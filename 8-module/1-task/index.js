c14d7a19bf4de0eb58f65f0fee66d648ff6d222f
Ivan <ivonchik996@gmail.com>
2024-01-31 20:32:30 +0300

    add  8-module-1-task

--- 8-module/1-task/index.js
+++ 8-module/1-task/index.js
@@ -1,4 +1,4 @@
-import createElement from '../../assets/lib/create-element.js';
+import createElement from "../../assets/lib/create-element.js";
 
 export default class CartIcon {
   constructor() {
@@ -13,7 +13,7 @@ export default class CartIcon {
 
   update(cart) {
     if (!cart.isEmpty()) {
-      this.elem.classList.add('cart-icon_visible');
+      this.elem.classList.add("cart-icon_visible");
 
       this.elem.innerHTML = `
         <div class="cart-icon__inner">
@@ -21,24 +21,45 @@ export default class CartIcon {
           <span class="cart-icon__price">€${cart.getTotalPrice().toFixed(2)}</span>
         </div>`;
 
+      this.initialTopCoord = this.elem.getBoundingClientRect().top + window.scrollY 
       this.updatePosition();
 
-      this.elem.classList.add('shake');
-      this.elem.addEventListener('transitionend', () => {
-        this.elem.classList.remove('shake');
-      }, {once: true});
-
+      this.elem.classList.add("shake");
+      this.elem.addEventListener(
+        "transitionend", () => {this.elem.classList.remove("shake");},
+        { once: true }
+      );
     } else {
-      this.elem.classList.remove('cart-icon_visible');
+      this.elem.classList.remove("cart-icon_visible");
     }
   }
 
   addEventListeners() {
-    document.addEventListener('scroll', () => this.updatePosition());
-    window.addEventListener('resize', () => this.updatePosition());
+    document.addEventListener("scroll", () => this.updatePosition());
+    window.addEventListener("resize", () => this.updatePosition());
   }
 
   updatePosition() {
-    // ваш код ...
+      let leftIndent = Math.min(
+        document.querySelector(".container").getBoundingClientRect().right + 20,
+        document.documentElement.clientWidth - this.elem.offsetWidth - 10
+      ) + "px";
+
+    if (this.elem.offsetWidth > 0 && window.scrollY  > this.initialTopCoord) {
+      Object.assign(this.elem.style, {
+        position: "fixed",
+        top: "50px",
+        zIndex: 1e3,
+        right:"10px",
+        left: leftIndent,
+      });
+    } else if (window.scrollY  < this.initialTopCoord || document.documentElement.clientWidth <= 767) {
+      Object.assign(this.elem.style, {
+        position: "",
+        top: "",
+        left: "",
+        zIndex: "",
+      }); 
+    } 
   }
 }
